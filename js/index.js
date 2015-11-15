0429c2b836e624e2f3ecd61a151a914116e5df9d
Marcelo Guará <mhguara@gmail.com>
2015-11-15 16:50:06 -0300

    Ajuste com quebra de linha na formatação.

--- js/index.js
+++ js/index.js
@@ -24,6 +24,7 @@ function format() {
 	conteudo = conteudo.replace(/(\.)\s/g, '$1<br />');
 	conteudo = conteudo.replace(/([a-zA-Z\u00C0-\u00FF ]+:)\s(\d)/gi,
 			'</p><p class="form-control-static"><strong>$1</strong><br />$2');
+	conteudo = conteudo.replace(/\n/g, ' ');
 	conteudo = '<p class="hidden">' + conteudo + '</p>';
 
 	conteudoformatado.html(conteudo);
