6bc288f861cf43bef89b2cb5a44fad68d6d559de
Marcelo Guará <mhguara@gmail.com>
2015-11-15 15:10:43 -0300

    Projeto para formatar o conteúdo programático do edital.

---
+++ js/index.js
@@ -0,0 +1,36 @@
+function initClipboard() {
+	var clipboard = new Clipboard('.btn');
+
+	clipboard.on('success', function(e) {
+		console.log(e);
+	});
+
+	clipboard.on('error', function(e) {
+		console.log(e);
+	});
+
+	return clipboard;
+}
+
+var clipboard = initClipboard();
+
+function format() {
+	var conteudonaoformatado = $('#conteudonaoformatado');
+	var conteudoformatado = $('#conteudoformatado');
+
+	var conteudo = conteudonaoformatado.val();
+
+	conteudo = conteudo.replace(/\s\s/g, ' ');
+	conteudo = conteudo.replace(/(\.)\s/g, '$1<br />');
+	conteudo = conteudo.replace(/([a-zA-Z\u00C0-\u00FF ]+:)\s(\d)/gi,
+			'</p><p class="form-control-static"><strong>$1</strong><br />$2');
+	conteudo = '<p class="hidden">' + conteudo + '</p>';
+
+	conteudoformatado.html(conteudo);
+
+	var conteudoformatadodiv = $('#conteudoformatadodiv');
+	conteudoformatadodiv.removeClass('hidden');
+
+	clipboard.destroy();
+	clipboard = initClipboard();
+}
